73ae849b2ef22af8f3101a4bb7add88a634e0493
Lubos Turek <lubos.turek@gmail.com>
2017-05-18 11:53:40 +0200

    Add listCrawlers and its tests

--- src/crawlers.js
+++ src/crawlers.js
@@ -1,1 +1,20 @@
-export default {};
+import _ from 'underscore';
+
+export const BASE_PATH = '/v1';
+
+export default {
+    listCrawlers: (requestPromise, options) => {
+        if (!options.userId) {
+            throw new Error('Missing required parameter: userId');
+        }
+        if (!options.token) {
+            throw new Error('Missing required parameter: token');
+        }
+
+        return requestPromise({
+            url: `${options.baseUrl}${BASE_PATH}/${options.userId}/crawlers?token=${options.token}`,
+            json: true,
+            method: 'GET',
+        });
+    },
+};
