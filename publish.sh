338fa8d9ab0a930c5b8af57586a932cf35d08d4b
Marek Trunkat <marek@trunkat.eu>
2017-07-20 12:07:20 +0200

    Updating publish.sh

--- publish.sh
+++ publish.sh
@@ -9,6 +9,7 @@ set -e
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+PACKAGE_NAME=`node -pe "require('./package.json').name"`
 PACKAGE_VERSION=`node -pe "require('./package.json').version"`
 BRANCH=`git status | grep 'On branch' | cut -d ' ' -f 3`
 BRANCH_UP_TO_DATE=`git status | grep 'nothing to commit' | tr -s \n ' '`;
@@ -25,10 +26,28 @@ if [ -z "$BRANCH_UP_TO_DATE" ]; then
     exit 1
 fi
 
+echo "Generating documentation ..."
+npm run build-doc
+
+echo "Pushing to git ..."
+git push
+
+# Master gets published as LATEST if that version doesn't exists yet and retagged as LATEST otherwise.
 if [ $BRANCH = "master" ]; then
-    NPM_TAG='latest'
+    if [ -z `npm view apify-client versions | grep $PACKAGE_VERSION` ]; then
+        echo "Publishing version ${PACKAGE_VERSION} with tag \"latest\" ..."
+        RUNNING_FROM_SCRIPT=1 npm publish --tag latest
+    else
+        echo "Tagging version ${PACKAGE_VERSION} with tag \"latest\" ..."
+        RUNNING_FROM_SCRIPT=1 dist-tag add ${PACKAGE_VERSION} latest
+    fi
+
+# Develop branch gets published as BETA and we don't allow to override tag of existing version.
 elif [ $BRANCH = "develop" ]; then
-    NPM_TAG='beta'
+    echo "Publishing version ${PACKAGE_VERSION} with tag \"beta\" ..."
+    RUNNING_FROM_SCRIPT=1 npm publish --tag beta
+
+# For other branch throw an error.
 else
     printf "${RED}You can publish from develop and master branches only!${NC}\n"
     exit 1
