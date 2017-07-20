385c375deda2fadbe13a73e4157d25d30bfd2ec2
Marek Trunkat <marek@trunkat.eu>
2017-07-20 11:46:10 +0200

    Increasing npm version

--- publish.sh
+++ publish.sh
@@ -49,6 +49,6 @@ git push origin ${GIT_TAG}
 echo "Git tag: ${GIT_TAG} created."
 
 echo "Uploading doc to s3 ..."
-AWS_ACCESS_KEY=${AWS_ACCESS_KEY} AWS_SECRET_KEY=${AWS_SECRET_KEY} AWS_BUCKET=${AWS_BUCKET} AWS_BUCKET_FOLDER=${GIT_TAG} node ./node_modules/deploy-web-to-s3/bin/deploy-web-to-s3.js ${TEMP_DOC_DIR}
+AWS_ACCESS_KEY=${AWS_ACCESS_KEY} AWS_SECRET_KEY=${AWS_SECRET_KEY} AWS_BUCKET=${AWS_BUCKET} AWS_BUCKET_FOLDER=${GIT_TAG} node ./node_modules/deploy-web-to-s3/bin/deploy-web-to-s3.js ${DOC_DIR}
 
 echo "Done."
