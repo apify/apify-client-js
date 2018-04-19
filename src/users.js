b78e23c072288bcacf5a934cec96d4581c6bff80
JakubDrobnik <drobnik.j@gmail.com>
2018-04-19 15:03:16 +0200

    Fix doc for users method #62

--- src/users.js
+++ src/users.js
@@ -1,20 +1,26 @@
 import { ME_USER_NAME_PLACEHOLDER } from 'apify-shared/consts';
 import { checkParamOrThrow, pluckData } from './utils';
 
+/**
+ * Users
+ * @memberOf ApifyClient
+ * @namespace users
+ */
+
 export const BASE_PATH = '/v2/users';
 
 export default {
     /**
-     * Returns public profile of user for given user ID or username.
-     * If user ID is not specified it returns info about your account.
+     * Returns private and public information about user account.
      *
      * @memberof ApifyClient.users
      * @instance
      * @param {Object} options
-     * @param {String} [options.token]
-     * @param {String} [options.userId = 'me'] - User ID or username of the user
-     * @param callback
-     * @returns {UserInfo}<
+     * @param {String} [options.token] - If is set, it shows private and public information about user account, otherwise only public.
+     * @param {String} [options.userId = 'me'] - User ID or username of the user,
+     *                                          if not set it returns information about current user account.
+     * @param {function} [callback] - Callback function
+     * @returns {UserInfo}
      */
     getUser: (requestPromise, options) => {
         const { baseUrl, userId = ME_USER_NAME_PLACEHOLDER, token } = options;
