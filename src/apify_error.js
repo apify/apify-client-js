fac5c659287aa9f0f32a14047b6b1ecc56b65414
Ondra Urban <ondrejurban@hotmail.com>
2018-08-08 15:45:55 +0200

    Fix typo

--- src/apify_error.js
+++ src/apify_error.js
@@ -1,7 +1,7 @@
 export const APIFY_ERROR_NAME = 'ApifyError';
 
 export const INVALID_PARAMETER_ERROR_TYPE_V1 = 'INVALID_PARAMETER';
-export const INVALID_PARAMETER_ERROR_TYPE_V2 = 'indalid-parameter';
+export const INVALID_PARAMETER_ERROR_TYPE_V2 = 'invalid-parameter';
 export const REQUEST_FAILED_ERROR_TYPE_V1 = 'REQUEST_FAILED';
 export const REQUEST_FAILED_ERROR_TYPE_V2 = 'request-failed';
 export const REQUEST_FAILED_ERROR_MESSAGE = 'Server request failed.';
