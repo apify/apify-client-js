2c0c1028e99612a232e6e4dbce9c9f3140448193
Jan Curn <jan.curn@gmail.com>
2018-09-19 10:40:33 +0200

    Updated docs

--- src/datasets.js
+++ src/datasets.js
@@ -227,26 +227,27 @@ export default {
      *   If `true` then the objects are sorted by `createdAt` in descending order.
      *   Otherwise they are sorted in ascending order.
      * @param {Array} [options.fields]
-     *   If provided then returned objects will only contain specified keys
+     *   An array of field names that will be included in the result. If omitted, all fields are included in the results.
      * @param {String} [options.unwind]
-     *   If provided then objects will be unwound based on provided field
+     *   Specifies a name of the field in the result objects that will be used to unwind the resulting objects.
+     *   By default, the results are returned as they are.
      * @param {Boolean} [options.disableBodyParser]
-     *   If true then response from API will not be parsed
+     *   If `true` then response from API will not be parsed
      * @param {Number} [options.attachment]
-     *   If 1 then the response will define the Content-Disposition: attachment header, forcing a web
-     *   browser to download the file rather than to display it. By default this header is not present.
+     *   If `true` then the response will define the `Content-Disposition: attachment` HTTP header, forcing a web
+     *   browser to download the file rather than to display it. By default, this header is not present.
      * @param {String} [options.delimiter=',']
-     *   A delimiter character for CSV files, only used if format=csv. You might need to URL-encode
-     *   the character (e.g. use %09 for tab or %3B for semicolon).
+     *   A delimiter character for CSV files, only used if `format` is `csv`.
+     *   You might need to URL-encode the character (e.g. use `%09` for tab or `%3B` for semicolon).
      * @param {Number} [options.bom]
-     *   All responses are encoded in UTF-8 encoding. By default, the csv files are prefixed with the UTF-8 Byte
-     *   Order Mark (BOM), while json, jsonl, xml, html and rss files are not. If you want to override this default
-     *   behavior, specify bom=1 query parameter to include the BOM or bom=0 to skip it.
+     *   All responses are encoded in UTF-8 encoding. By default, the CSV files are prefixed with the UTF-8 Byte
+     *   Order Mark (BOM), while JSON, JSONL, XML, HTML and RSS files are not. If you want to override this default
+     *   behavior, set `bom` option to `true` to include the BOM, or set `bom` to `false` to skip it.
      * @param {String} [options.xmlRoot]
-     *   Overrides default root element name of xml output. By default the root element is results.
+     *   Overrides the default root element name of the XML output. By default, the root element is `results`.
      * @param {String} [options.xmlRow]
-     *   Overrides default element name that wraps each page or page function result object in xml output.
-     *   By default the element name is page or result based on value of simplified parameter.
+     *   Overrides the default element name that wraps each page or page function result object in XML output.
+     *   By default, the element name is `page` or `result`, depending on the value of the `simplified` option.
      * @param {Number} [options.skipHeaderRow]
      *   If set to `1` then header row in csv format is skipped.
      * @param {String} [options.token]
