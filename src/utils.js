899e20c1d71cf831474941ab648d838ec0867be2
Jaroslav Hejlek <hejlekjaroslav@gmail.com>
2019-04-11 11:43:23 +0200

    Fixed wrong iteration index in requestPromise function

--- src/utils.js
+++ src/utils.js
@@ -120,8 +120,8 @@ export const requestPromise = async (options, stats) => {
 
         if (statusCode === RATE_LIMIT_EXCEEDED_STATUS_CODE && stats) {
             // Make sure this doesn't fail when someone increases number of retries on anything.
-            if (typeof stats.rateLimitErrors[iteration] === 'number') stats.rateLimitErrors[iteration]++;
-            else stats.rateLimitErrors[iteration] = 1;
+            if (typeof stats.rateLimitErrors[iteration - 1] === 'number') stats.rateLimitErrors[iteration - 1]++;
+            else stats.rateLimitErrors[iteration - 1] = 1;
         }
 
         // For status codes 300-499 except RATE_LIMIT_EXCEEDED_STATUS_CODE we immediately rejects the promise
