7976527a369f776fb10018f86676ae82792eebf0
Jan Curn <jan.curn@gmail.com>
2019-09-12 18:25:15 +0200

    Improved Datasets.getItems() docs

--- src/datasets.js
+++ src/datasets.js
@@ -226,12 +226,12 @@ export default {
      * @param {String} options.datasetId
      *   Unique dataset ID
      * @param {String} [options.format='json']
-     *   Format of the items, possible values are: json, csv, xlsx, html, xml and rss.
+     *   Format of the `items` property, possible values are: `json`, `csv`, `xlsx`, `html`, `xml` and `rss`.
      * @param {Number} [options.offset=0]
      *   Number of array elements that should be skipped at the start.
-     * @param {Number} [options.limit=100000]
+     * @param {Number} [options.limit=250000]
      *   Maximum number of array elements to return.
-     * @param {Number} [options.desc]
+     * @param {Boolean} [options.desc=false]
      *   If `true` then the objects are sorted by `createdAt` in descending order.
      *   Otherwise they are sorted in ascending order.
      * @param {Array} [options.fields]
@@ -239,40 +239,39 @@ export default {
      * @param {String} [options.unwind]
      *   Specifies a name of the field in the result objects that will be used to unwind the resulting objects.
      *   By default, the results are returned as they are.
-     * @param {Boolean} [options.disableBodyParser]
-     *   If `true` then response from API will not be parsed
-     * @param {Number} [options.attachment]
+     * @param {Boolean} [options.disableBodyParser=false]
+     *   If `true` then response from API will not be parsed.
+     * @param {Boolean} [options.attachment=false]
      *   If `true` then the response will define the `Content-Disposition: attachment` HTTP header, forcing a web
      *   browser to download the file rather than to display it. By default, this header is not present.
      * @param {String} [options.delimiter=',']
      *   A delimiter character for CSV files, only used if `format` is `csv`.
-     *   You might need to URL-encode the character (e.g. use `%09` for tab or `%3B` for semicolon).
-     * @param {Number} [options.bom]
+     * @param {Boolean} [options.bom]
      *   All responses are encoded in UTF-8 encoding. By default, the CSV files are prefixed with the UTF-8 Byte
      *   Order Mark (BOM), while JSON, JSONL, XML, HTML and RSS files are not. If you want to override this default
      *   behavior, set `bom` option to `true` to include the BOM, or set `bom` to `false` to skip it.
-     * @param {String} [options.xmlRoot]
+     * @param {String} [options.xmlRoot='results']
      *   Overrides the default root element name of the XML output. By default, the root element is `results`.
-     * @param {String} [options.xmlRow]
+     * @param {String} [options.xmlRow='page']
      *   Overrides the default element name that wraps each page or page function result object in XML output.
      *   By default, the element name is `page` or `result`, depending on the value of the `simplified` option.
-     * @param {Boolean} [options.skipHeaderRow]
-     *   If set to `true` then header row in csv format is skipped.
-     * @param {Boolean} [options.clean]
+     * @param {Boolean} [options.skipHeaderRow=false]
+     *   If set to `true` then header row in CSV format is skipped.
+     * @param {Boolean} [options.clean=false]
      *   If `true` then the function returns only non-empty items and skips hidden fields (i.e. fields starting with `#` character).
      *   Note that the `clean` parameter is a shortcut for `skipHidden: true` and `skipEmpty: true` options.
-     * @param {Boolean} [options.skipHidden]
+     * @param {Boolean} [options.skipHidden=false]
      *   If `true` then the function doesn't return hidden fields (fields starting with "#" character).
-     * @param {Boolean} [options.skipEmpty]
+     * @param {Boolean} [options.skipEmpty=false]
      *   If `true` then the function doesn't return empty items.
      *   Note that in this case the returned number of items might be lower than limit parameter and pagination must be done using the `limit` value.
      * @param {Boolean} [options.simplified]
-     *   If true then, the function applies the `fields: ['url','pageFunctionResult','errorInfo']` and `unwind: 'pageFunctionResult'` options.
-     *   This feature is used to emulate simplified results provided by the legacy Apify Crawler product
-     *   and it's not recommended to use it in new integrations.
+     *   If `true` then function applies the `fields: ['url','pageFunctionResult','errorInfo']` and `unwind: 'pageFunctionResult'` options.
+     *   This feature is used to emulate simplified results provided by Apify API version 1 used for
+     *   the legacy Apify Crawler and it's not recommended to use it in new integrations.
      * @param {Boolean} [options.skipFailedPages]
-     *   If true then, the all the items with errorInfo property will be skipped from the output.
-     *   This feature is here to emulate functionality of API v1 used for
+     *   If `true` then, the all the items with errorInfo property will be skipped from the output.
+     *   This feature is here to emulate functionality of Apify API version 1 used for
      *   the legacy Apify Crawler product and it's not recommended to use it in new integrations.
      * @param {String} [options.token]
      *   Your API token at apify.com. This parameter is required
