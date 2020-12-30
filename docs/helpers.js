78477dceae7bea5b87a6598c2cbc2e2a6e77bb31
Misha <m.murasovs@gmail.com>
2020-12-30 13:19:15 +0100

    Add helpers

--- docs/helpers.js
+++ docs/helpers.js
@@ -1,37 +1,4 @@
-const ddata = require('dmd/helpers/ddata'); // eslint-disable-line
-
-/* eslint-disable no-underscore-dangle */
-
-exports.inlineLinks = (text) => {
-    if (text) {
-        const links = ddata.parseLink(text);
-        links.forEach((link) => {
-            // const linked = ddata._link(link.url, options);
-            // if (link.caption === link.url) link.caption = linked.name;
-            // if (linked.url) link.url = linked.url;
-            // const url = link.url.includes('+') ? link.url : link.url.toLowerCase();
-            text = text.replace(link.original, `LINK!${link.caption}!LINK`);
-        });
-    }
-    return text;
-};
-
-exports.escape = function escape(input) {
-    if (typeof input !== 'string') return null;
-    return input.replace(/([*|])/g, '$1');
-};
-
 exports.lowerCase = (text) => text.toLowerCase();
-
-exports.class = (options) => {
-    options.hash.kind = 'class';
-    const result = ddata._identifier(options);
-    if (result && result.kind === 'class') {
-        result.scope = 'global';
-        delete result.memberof;
-    }
-    return result ? options.fn(result) : 'ERROR, Cannot find class.';
-};
 
 exports.params = function params(options) {
     if (this.params) {
