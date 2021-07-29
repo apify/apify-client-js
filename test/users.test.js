5faa63cdb65af7a92ca66151b3c481d775fa2b75
drobnikj <drobnik.j@gmail.com>
2021-07-29 15:07:12 +0200

    feat: changed passing token using request HTTP header instead of the request parameter for every API calls

--- test/users.test.js
+++ test/users.test.js
@@ -1,7 +1,7 @@
 const { ME_USER_NAME_PLACEHOLDER } = require('@apify/consts');
 const ApifyClient = require('../src');
 const mockServer = require('./mock_server/server');
-const { Browser, validateRequest, DEFAULT_QUERY } = require('./_helper');
+const { Browser, validateRequest, DEFAULT_OPTIONS } = require('./_helper');
 
 describe('User methods', () => {
     let baseUrl;
@@ -23,11 +23,11 @@ describe('User methods', () => {
     let client;
     let page;
     beforeEach(async () => {
-        page = await browser.getInjectedPage(baseUrl, DEFAULT_QUERY);
+        page = await browser.getInjectedPage(baseUrl, DEFAULT_OPTIONS);
         client = new ApifyClient({
             baseUrl,
             maxRetries: 0,
-            ...DEFAULT_QUERY,
+            ...DEFAULT_OPTIONS,
         });
     });
     afterEach(async () => {
