f846d3f48cc9c655fba9cea04ac3a5f6daeb18be
Vlad Frangu <kingdgrizzle@gmail.com>
2021-08-23 17:38:47 +0200

    refactor: convert webhooks to TS

---
+++ src/resource_clients/webhook_dispatch.ts
@@ -0,0 +1,47 @@
+import { ApiClientSubResourceOptions } from '../base/api_client';
+import { ResourceClient } from '../base/resource_client';
+import { Webhook, WebhookEventType } from './webhook';
+
+/**
+ * @hideconstructor
+ */
+export class WebhookDispatchClient extends ResourceClient {
+    constructor(options: ApiClientSubResourceOptions) {
+        super({
+            resourcePath: 'webhook-dispatches',
+            ...options,
+        });
+    }
+
+    /**
+     * https://docs.apify.com/api/v2#/reference/webhook-dispatches/webhook-dispatch-object/get-webhook-dispatch
+     */
+    async get(): Promise<WebhookDispatch | undefined> {
+        return this._get();
+    }
+}
+
+export interface WebhookDispatch {
+    id: string;
+    userId: string;
+    webhookId: string;
+    createdAt: string;
+    status: WebhookDispatchStatus;
+    eventType: WebhookEventType;
+    calls: WebhookDispatchCall[];
+    webhook: Pick<Webhook, 'requestUrl' | 'isAdHoc'>;
+}
+
+export enum WebhookDispatchStatus {
+    Active = 'ACTIVE',
+    Succeeded = 'SUCCEEDED',
+    Failed = 'FAILED',
+}
+
+export interface WebhookDispatchCall {
+    startedAt: string;
+    finishedAt: string;
+    errorMessage: string | null;
+    responseStatus: number | null;
+    responseBody: string | null;
+}
