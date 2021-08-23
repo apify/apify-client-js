651faa185d548945746e27ec989fead8c21962ed
Vlad Frangu <kingdgrizzle@gmail.com>
2021-08-23 14:00:26 +0200

    refactor: convert schedule to TS

---
+++ src/resource_clients/schedule.ts
@@ -0,0 +1,127 @@
+import ow from 'ow';
+import ApifyApiError from '../apify_api_error';
+import { ApiClientSubResourceOptions } from '../base/api_client';
+import { ResourceClient } from '../base/resource_client';
+import { Timezone } from '../timezones';
+import {
+    pluckData,
+    parseDateFields,
+    catchNotFoundOrThrow,
+    cast,
+} from '../utils';
+
+/**
+ * @hideconstructor
+ */
+export class ScheduleClient extends ResourceClient {
+    constructor(options: ApiClientSubResourceOptions) {
+        super({
+            resourcePath: 'schedules',
+            ...options,
+        });
+    }
+
+    /**
+     * https://docs.apify.com/api/v2#/reference/schedules/schedule-object/get-schedule
+     */
+    async get(): Promise<Schedule | undefined> {
+        return this._get();
+    }
+
+    /**
+     * https://docs.apify.com/api/v2#/reference/schedules/schedule-object/update-schedule
+     */
+    async update(newFields: ScheduleUpdateData): Promise<Schedule> {
+        ow(newFields, ow.object);
+        return this._update(newFields);
+    }
+
+    /**
+     * https://docs.apify.com/api/v2#/reference/schedules/schedule-object/delete-schedule
+     */
+    async delete(): Promise<void> {
+        return this._delete();
+    }
+
+    /**
+     * https://docs.apify.com/api/v2#/reference/schedules/schedule-log/get-schedule-log
+     */
+    async getLog(): Promise<string | undefined> {
+        const requestOpts = {
+            url: this._url('log'),
+            method: 'GET',
+            params: this._params(),
+        };
+        try {
+            const response = await this.httpClient.call(requestOpts);
+            return cast(parseDateFields(pluckData(response.data)));
+        } catch (err) {
+            catchNotFoundOrThrow(err as ApifyApiError);
+        }
+
+        return undefined;
+    }
+}
+
+export interface Schedule {
+    id: string;
+    userId: string;
+    name: string;
+    cronExpression: string;
+    timezone: Timezone;
+    isEnabled: boolean;
+    isExclusive: boolean;
+    description?: string;
+    createdAt: string;
+    modifiedAt: string;
+    nextRunAt: string;
+    lastRunAt: string;
+    actions: ScheduleAction[];
+}
+
+export type ScheduleUpdateData = Partial<
+    Pick<
+        Schedule,
+        | 'name'
+        | 'cronExpression'
+        | 'timezone'
+        | 'isEnabled'
+        | 'isExclusive'
+        | 'description'
+        | 'actions'
+    >
+>;
+
+export enum ScheduleActions {
+    RunActor = 'RUN_ACTOR',
+    RunActorTask = 'RUN_ACTOR_TASK',
+}
+
+interface BaseScheduleAction<Type extends ScheduleActions> {
+    id: string;
+    type: Type;
+}
+
+export type ScheduleAction = ScheduleActionRunActor | ScheduleActionRunActorTask;
+
+export interface ScheduleActionRunActor extends BaseScheduleAction<ScheduleActions.RunActor> {
+    actorId: string;
+    runInput?: ScheduledActorRunInput;
+    runOptions?: ScheduledActorRunOptions;
+}
+
+export interface ScheduledActorRunInput {
+    body: string;
+    contentType: string;
+}
+
+export interface ScheduledActorRunOptions {
+    build: string;
+    timeoutSecs: number;
+    memoryMbytes: number;
+}
+
+export interface ScheduleActionRunActorTask extends BaseScheduleAction<ScheduleActions.RunActorTask> {
+    actorTaskId: string;
+    input?: string;
+}
