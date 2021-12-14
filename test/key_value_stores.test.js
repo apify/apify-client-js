71bfa17ed7ffbf1359e4fd06e7e632185aba4595
Ondra Urban <ondra@apify.com>
2021-12-14 19:01:22 +0100

    fix: unnamed storages

--- test/key_value_stores.test.js
+++ test/key_value_stores.test.js
@@ -53,7 +53,17 @@ describe('Key-Value Store methods', () => {
             validateRequest(opts);
         });
 
-        test('getOrCreate() works', async () => {
+        test('getOrCreate() works without name', async () => {
+            const res = await client.keyValueStores().getOrCreate();
+            expect(res.id).toEqual('get-or-create-store');
+            validateRequest();
+
+            const browserRes = await page.evaluate((n) => client.keyValueStores().getOrCreate(n));
+            expect(browserRes).toEqual(res);
+            validateRequest();
+        });
+
+        test('getOrCreate() works with name', async () => {
             const name = 'some-id-2';
 
             const res = await client.keyValueStores().getOrCreate(name);
