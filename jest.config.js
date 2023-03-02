6cfc26447760b29fda3a5c2129058216aeed5d2a
Martin Adámek <banan23@gmail.com>
2023-03-02 13:04:48 +0100

    test: simplify jest config and use JS
    TS configs in jest are type checked during load via ts-node so it's slower to use them.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
-import type { Config } from '@jest/types';
-
-export default async (): Promise<Config.InitialOptions> => ({
+module.exports = {
     verbose: true,
     preset: 'ts-jest',
     testEnvironment: 'node',
@@ -13,9 +11,7 @@ export default async (): Promise<Config.InitialOptions> => ({
     testTimeout: 20_000,
     collectCoverage: true,
     collectCoverageFrom: [
-        'src/**/*.ts',
-        'src/**/*.js',
-        '!**/node_modules/**',
+        '<rootDir>/src/**/*.[jt]s',
     ],
     maxWorkers: 3,
-});
+};
