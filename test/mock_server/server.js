cd4386dcda8f4c9553056938828e181401bb9387
Martin Adámek <banan23@gmail.com>
2023-03-02 12:26:39 +0100

    chore: update dev dependencies (#326)

--- test/mock_server/server.js
+++ test/mock_server/server.js
@@ -2,7 +2,6 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const logger = require('morgan');
 const compression = require('compression');
 
 // Routers
@@ -58,7 +57,6 @@ const mockServer = {
 app.use((req, res, next) => {
     next();
 });
-app.use(logger('dev'));
 app.use(express.text());
 app.use(express.json({ limit: '9mb' }));
 app.use(express.urlencoded({ extended: false }));
