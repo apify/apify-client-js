05742540b7258f3f1524bbeda27f4d34f43728dd
Sviatozar Petrenko <svpetrenko123@gmail.com>
2023-11-28 17:11:40 +0200

    Update src/resource_clients/log.ts
    Co-authored-by: Vlad Frangu <kingdgrizzle@gmail.com>

--- src/resource_clients/log.ts
+++ src/resource_clients/log.ts
@@ -1,4 +1,4 @@
-import { Readable } from 'stream';
+import { Readable } from 'node:stream';
 import { ApifyApiError } from '../apify_api_error';
 import { ApiClientSubResourceOptions } from '../base/api_client';
 import { ResourceClient } from '../base/resource_client';
