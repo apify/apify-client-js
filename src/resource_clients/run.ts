7e9def71e1e8b72f023dd06a45b9bcf946e4f945
Jan Kuzelik <jan.kuzelik@apify.com>
2024-12-05 11:51:12 +0100

    Add random suffix to idempotency key

--- src/resource_clients/run.ts
+++ src/resource_clients/run.ts
@@ -152,7 +152,9 @@ export class RunClient extends ResourceClient {
         }));
 
         const count = options.count ?? 1;
-        const idempotencyKey = options.idempotencyKey ?? `${this.id}-${options.eventName}-${Date.now()}`;
+        /** To avoid duplicates during the same milisecond, doesn't need to by crypto-secure. */
+        const randomSuffix = (Math.random() + 1).toString(36).slice(3, 8);
+        const idempotencyKey = options.idempotencyKey ?? `${this.id}-${options.eventName}-${randomSuffix}`;
 
         const request: AxiosRequestConfig = {
             url: this._url('charge'),
