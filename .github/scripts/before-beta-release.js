29207698ef3417d02cc7ee51fbc0a44455f2c505
Jan Buchar <jan.buchar@apify.com>
2025-05-13 16:01:21 +0200

    ci: Fix pre-release version handling (#694)

--- .github/scripts/before-beta-release.js
+++ .github/scripts/before-beta-release.js
@@ -11,25 +11,25 @@ const pkgJson = require(PKG_JSON_PATH);
 const PACKAGE_NAME = pkgJson.name;
 const VERSION = pkgJson.version;
 
-const nextVersion = getNextVersion(VERSION);
+const nextVersion = addBetaSuffixToVersion(VERSION);
 console.log(`before-deploy: Setting version to ${nextVersion}`);
 pkgJson.version = nextVersion;
 
 fs.writeFileSync(PKG_JSON_PATH, `${JSON.stringify(pkgJson, null, 2)}\n`);
 
-function getNextVersion(version) {
+function addBetaSuffixToVersion(version) {
     const versionString = execSync(`npm show ${PACKAGE_NAME} versions --json`, { encoding: 'utf8' });
     const versions = JSON.parse(versionString);
 
-    if (versions.some((v) => v === VERSION)) {
+    if (versions.some((v) => v === version)) {
         console.error(
-            `before-deploy: A release with version ${VERSION} already exists. Please increment version accordingly.`,
+            `before-deploy: A release with version ${version} already exists. Please increment version accordingly.`,
         );
         process.exit(1);
     }
 
     const prereleaseNumbers = versions
-        .filter((v) => v.startsWith(VERSION) && v.includes('-'))
+        .filter((v) => v.startsWith(version) && v.includes('-'))
         .map((v) => Number(v.match(/\.(\d+)$/)[1]));
     const lastPrereleaseNumber = Math.max(-1, ...prereleaseNumbers);
     return `${version}-beta.${lastPrereleaseNumber + 1}`;
