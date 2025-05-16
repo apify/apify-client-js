be25137575435547aaf2c3849fc772daf0537450
Martin Forejt <forejt.martin97@gmail.com>
2025-05-16 12:29:02 +0200

    feat: list kv store keys by collection of prefix (#688)
    Add `collection` and `prefix` query params for listing key-value store
    keys based on
    https://github.com/apify/actor-whitepaper/blob/master/pages/KEY_VALUE_STORE_SCHEMA.md#api-implications
    
    Api docs: https://github.com/apify/apify-docs/pull/1564
    Api impl: https://github.com/apify/apify-core/pull/20726

--- src/resource_clients/key_value_store.ts
+++ src/resource_clients/key_value_store.ts
@@ -55,6 +55,8 @@ export class KeyValueStoreClient extends ResourceClient {
             ow.object.exactShape({
                 limit: ow.optional.number,
                 exclusiveStartKey: ow.optional.string,
+                collection: ow.optional.string,
+                prefix: ow.optional.string,
             }),
         );
 
@@ -267,6 +269,8 @@ export interface KeyValueClientUpdateOptions {
 export interface KeyValueClientListKeysOptions {
     limit?: number;
     exclusiveStartKey?: string;
+    collection?: string;
+    prefix?: string;
 }
 
 export interface KeyValueClientListKeysResult {
