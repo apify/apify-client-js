14464e3a2b63d0f2a8c74da3736972ccd2bf471c
Martin Adámek <banan23@gmail.com>
2025-07-30 18:17:16 +0200

    docs: respect scripts and customFields from shared config (#721)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -14,6 +14,7 @@ module.exports = {
     organizationName: 'apify',
     projectName: 'apify-client-js',
     favicon: 'img/favicon.ico',
+    scripts: [...(config.scripts ?? [])],
     onBrokenLinks: /** @type {import('@docusaurus/types').ReportingSeverity} */ ('throw'),
     onBrokenMarkdownLinks: /** @type {import('@docusaurus/types').ReportingSeverity} */ ('throw'),
     future: {
@@ -112,4 +113,7 @@ module.exports = {
     ],
     themeConfig: { ...config.themeConfig, versions },
     staticDirectories: ['node_modules/@apify/docs-theme/static', 'static'],
+    customFields: {
+        ...(config.customFields ?? []),
+    },
 };
