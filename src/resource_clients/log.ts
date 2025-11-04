0b2e6e6cabde4b65818c09e633bb79b5d4f689b9
Josef Prochazka <josef.prochazka@apify.com>
2025-11-04 16:19:32 +0100

    Simplify handling of multibyte chars

--- src/resource_clients/log.ts
+++ src/resource_clients/log.ts
@@ -170,52 +170,27 @@ export class StreamedLog {
      * Get log stream from response and redirect it to another log.
      */
     private async streamLog(): Promise<void> {
-        const twoBytesLimit = 0xc0;
-        const threeBytesLimit = 0xe0;
-        const fourBytesLimit = 0xf0;
-
         const logStream = await this.logClient.stream({ raw: true });
         if (!logStream) {
             return;
         }
-        let incompleteCharacter: Uint8Array = new Uint8Array();
+        let previousChunkRemainder: Uint8Array = new Uint8Array();
 
         for await (const chunk of logStream) {
-            // Handle possible leftover incomplete multibyte character from previous chunk
-            const chunkPrependedWithIncompleteCharacter = new Uint8Array(incompleteCharacter.length + chunk.length);
-            chunkPrependedWithIncompleteCharacter.set(incompleteCharacter, 0);
-            chunkPrependedWithIncompleteCharacter.set(chunk, incompleteCharacter.length);
+            // Handle possible leftover incomplete line from previous chunk.
+            // Everything before last end of line is complete.
+            const chunkWithPreviousRemainder = new Uint8Array(previousChunkRemainder.length + chunk.length);
+            chunkWithPreviousRemainder.set(previousChunkRemainder, 0);
+            chunkWithPreviousRemainder.set(chunk, previousChunkRemainder.length);
 
-            // Extract possible incomplete multibyte character from the end of this chunk
-            if (
-                chunkPrependedWithIncompleteCharacter.length > 1 &&
-                chunkPrependedWithIncompleteCharacter[chunkPrependedWithIncompleteCharacter.length] >= twoBytesLimit
-            ) {
-                incompleteCharacter = chunkPrependedWithIncompleteCharacter.slice(
-                    chunkPrependedWithIncompleteCharacter.length - 1,
-                );
-            } else if (
-                chunkPrependedWithIncompleteCharacter.length > 2 &&
-                chunkPrependedWithIncompleteCharacter[chunkPrependedWithIncompleteCharacter.length] >= threeBytesLimit
-            ) {
-                incompleteCharacter = chunkPrependedWithIncompleteCharacter.slice(
-                    chunkPrependedWithIncompleteCharacter.length - 2,
-                );
-            } else if (
-                chunkPrependedWithIncompleteCharacter.length > 3 &&
-                chunkPrependedWithIncompleteCharacter[chunkPrependedWithIncompleteCharacter.length] >= fourBytesLimit
-            ) {
-                incompleteCharacter = chunkPrependedWithIncompleteCharacter.slice(
-                    chunkPrependedWithIncompleteCharacter.length - 3,
-                );
-            }
+            const lastCompleteMessageIndex = chunkWithPreviousRemainder.lastIndexOf(0x0a);
+            previousChunkRemainder = chunkWithPreviousRemainder.slice(lastCompleteMessageIndex);
+
+            // Push complete part of the chunk to the buffer
+            this.streamBuffer.push(Buffer.from(chunkWithPreviousRemainder.slice(0, lastCompleteMessageIndex)));
+            this.logBufferContent(false);
 
             // Keep processing the new data until stopped
-            this.streamBuffer.push(Buffer.from(chunkPrependedWithIncompleteCharacter));
-            // Log data only if the chunk contains the marker as it indicates previous message is complete
-            if (this.splitMarker.test(chunkPrependedWithIncompleteCharacter.toString())) {
-                this.logBufferContent(false);
-            }
             if (this.stopLogging) {
                 break;
             }
@@ -258,27 +233,9 @@ export class StreamedLog {
             }
             const message = decodedMarker + decodedContent;
 
-            // Log parsed message at guessed level.
-            this.logAtGuessedLevel(message);
+            // Original log level information is not available. Log all on info level. Log level could be guessed for
+            // some logs, but for any multiline logs such guess would be probably correct only for the first line.
+            this.toLog.info(message.trim());
         });
-    }
-
-    /**
-     * Log messages at appropriate log level guessed from the message content.
-     *
-     * Original log level information does not have to be included in the message at all.
-     * This is methods just guesses, exotic formating or specific keywords can break the guessing logic.
-     */
-    private logAtGuessedLevel(message: string) {
-        message = message.trim();
-
-        if (message.includes('ERROR')) this.toLog.error(message);
-        else if (message.includes('SOFT_FAIL')) this.toLog.softFail(message);
-        else if (message.includes('WARNING')) this.toLog.warning(message);
-        else if (message.includes('INFO')) this.toLog.info(message);
-        else if (message.includes('DEBUG')) this.toLog.debug(message);
-        else if (message.includes('PERF')) this.toLog.perf(message);
-        // Fallback in case original log message does not indicate known log level.
-        else this.toLog.info(message);
     }
 }
