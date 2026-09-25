245d65bc3f4647f035ebca9fa67a00bc407a5b3b
Vlada Dusek <v.dusek96@gmail.com>
2026-09-25 11:16:58 +0200

    docs: fix inaccuracies found in review

--- docs/02_concepts/code/03_invalid_arguments.ts
+++ docs/02_concepts/code/03_invalid_arguments.ts
@@ -7,7 +7,7 @@ try {
     await client.dataset('my-dataset').listItems({ limit: 'ten' });
 } catch (error) {
     if (error instanceof ArgumentValidationError) {
-        // Invalid input: expected number, received string at `limit`, got `ten`
+        // Invalid input: expected number, received the string `ten` at `limit` in `DatasetClientListItemOptions`
         console.log(error.message);
         // [{ code: 'invalid_type', expected: 'number', path: ['limit'], ... }]
         console.log(error.issues);
